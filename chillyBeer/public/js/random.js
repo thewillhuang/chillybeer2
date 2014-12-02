6c450185d158d6c66a4b21767e1a44f7bfd75403
william huang <will.h86@gmail.com>
2014-12-01 18:35:06 -0800

    all files

---
+++ chillyBeer/public/js/random.js
@@ -0,0 +1,6 @@
+'use strict';
+var random = function() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+};
+
+module.exports = random;
---
+++ chillyBeer/public/js/squared.js
@@ -0,0 +1,6 @@
+'use strict';
+var square = function(num) {
+  return num * num;
+};
+
+module.exports = square;
